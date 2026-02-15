ee08213fa25b627f290ed96457fee7b2a2316dc3
Thanmay121 <thanmaysalt@gmail.com>
2026-02-15 21:34:22 +0530

    Updated pylist.c

--- pylist.c
+++ pylist.c
@@ -1,26 +1,45 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 typedef struct{
     int *data; 
     int size;
     int totalcap;
-} list;
+} createlist;
 
-list *newlist(int initalcapacity) {
-    list *ls1= malloc(sizeof(list));
-    (*ls1).data = malloc(initalcapacity*sizeof*(*ls1).data);
+void append(createlist *l1, int value);
+
+createlist *list(int initialcapacity, ...) {
+    createlist *ls1 = malloc(sizeof(createlist));
+    (*ls1).data = malloc(initialcapacity * sizeof *(*ls1).data);
     (*ls1).size = 0;
-    (*ls1).totalcap = initalcapacity;
+    (*ls1).totalcap = initialcapacity;
+
+    if (initialcapacity > 0) {
+        va_list args;
+        va_start(args, initialcapacity);
+
+        for (int i = 0; i < initialcapacity; i++) {
+            int x = va_arg(args, int);
+            append(ls1, x);
+        }
+
+        va_end(args);
+    }
+
     return ls1;
 }
-void append(list *l1, int value) 
+
+void append(createlist *l1, int value) 
 {
     if((*l1).size == (*l1).totalcap)
     {
-        int new_capacity = (*l1).totalcap+1; 
+        int new_capacity = (*l1).totalcap + 1; 
         if (new_capacity == 0) new_capacity = 1;
 
-        int *new_data = realloc((*l1).data, new_capacity * sizeof *(*l1).data);
+        int *new_data = realloc((*l1).data,
+                                new_capacity * sizeof *(*l1).data);
         (*l1).data = new_data;
         (*l1).totalcap = new_capacity; 
     }
@@ -29,10 +48,18 @@ void append(list *l1, int value)
     (*l1).size++;
 }
 
-int get(list *l1, int index) {
+int get(createlist *l1, int index) {
+    return (*l1).data[index];
+}
+
+void free_list(createlist *l1) {
+    free((*l1).data);
+    free(l1);
+}
     return (*l1).data[index];
 }
 void free_list(list *l1) {
     free((*l1).data);
     free(l1);
 }
+
